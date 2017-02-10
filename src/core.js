2378be687cdc46879593b773ba4ce87a2b70923d
Tin Crnkovic <tinc2k@gmail.com>
2017-02-10 19:39:13 +0100

    Request parsing & logging. HTTP response functions. Unhandled exception, unhandled reject, exit, SIGINT handling. Build & dev scripts in package.

--- src/core.js
+++ src/core.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const http = require('http'),
+const fs = require('graceful-fs'),
+      http = require('http'),
       https = require('https'),
-      fs = require('graceful-fs'),
+      parser = require('body-parser'),
       express = require('express'),
       cluster = require('cluster'),
       os = require('os'),
@@ -14,48 +15,55 @@ const config = require('./config'),
       Logger = require('./logger');
 
 const log = new Logger('Core'),
-      numCPUs = os.cpus().length,
-      portHttp = config.isDevelopment ? 3000 : 80,
-      portHttps = 443,
-      keepAlive = 300000; // 5 minutes
+      CPU_COUNT = os.cpus().length,
+      PORT_HTTP = config.isDevelopment ? 3000 : 80,
+      PORT_HTTPS = 443,
+      KEEP_ALIVE = 300000, // 5 minutes
+      JSON_LIMIT = '1mb'; // default '100kb'
 
 if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < CPU_COUNT; i++) {
     cluster.fork();
-  }
+  } 
   cluster.on('message', onWorkerMessage);
   cluster.on('online', onWorkerOnline)
   cluster.on('exit', onWorkerExit);
 } else {
   var app = express();
+  app.use(parser.urlencoded({ extended: true }));
+  app.use(parser.json({ limit: JSON_LIMIT }));
 
   if (config.isDevelopment) {
     // nice tidy JSON output, for better readability in dev
     app.set('json spaces', 2);
     // disable jade(pug) minification
     app.locals.pretty = true;
-  } else {
-    app.use(forceHttps);
   }
-  // isn't this a neat little path trick?
-  app.use('/static', express.static(__dirname + '/../static/'));
 
   const httpServer = http.createServer(app);
-  httpServer.on('connection', socket => socket.setTimeout(keepAlive));
-  httpServer.listen(portHttp);
+  httpServer.on('connection', socket => socket.setTimeout(KEEP_ALIVE));
+  httpServer.listen(PORT_HTTP);
 
-  if (!config.isDevelopment) {
+  if (config.SERVER.force_https) {
+    _logger.debug('Force HTTPS enabled.')
     const httpsServer = https.createServer({
         cert: fs.readFileSync(config.SERVER.certificate_path),
         key: fs.readFileSync(config.SERVER.certificate_key_path)
       }, app);
-    httpsServer.on('connection', socket => socket.setTimeout(keepAlive));
-    httpsServer.listen(portHttps);  
+    httpsServer.on('connection', socket => socket.setTimeout(KEEP_ALIVE));
+    httpsServer.listen(PORT_HTTPS);
+    app.use(forceHttps);
   }
 
-  app.get('/', (req, res) => {
-    log.debug('Hit endpoint.');
+    // isn't this a neat little path trick?
+  app.use('/static', express.static(__dirname + '/../static/'));
+
+  app.get('/', logRequest, (req, res) => {
     res.send('Hello World!')
+  });
+
+  app.use(logRequest, (req, res) => {
+    returnNotFound(res);
   });
 }
 
@@ -83,3 +91,68 @@ function forceHttps(req, res, next) {
     res.end();
   } else next();
 }
+
+function returnBadRequest(res, message) {
+  res.status(400).send(message ? message : 'Bad request.\n');
+  log.warn('Returned 400 Bad Request.', message);
+}
+
+function returnUnauthorized(res, message) {
+  res.status(401).send(message ? message : 'Unauthorized.\n');
+  log.warn('Returned 401 Unauthorized.', message);
+}
+
+function returnNotFound(res, message) {
+  res.status(404).send(message ? message : 'Not found.\n');
+  log.warn('Returned 404 Not Found.', message);
+}
+
+function returnTooLarge(res, message) {
+  res.status(413).send(message ? message : 'Payload too large.\n');
+  log.warn('Returned 413 Payload Too Large.', message);
+}
+
+function returnNotImplemented(res, message) {
+  res.status(501).send(message ? message : 'Not implemented.\n');
+  log.warn('Returned 501 Not Implemented.', message);
+}
+
+function returnServerError(res, message) {
+  res.status(500).send(message ? message : 'Server error.\n');
+  log.warn('Returned 500 Server Error.', message);
+}
+
+function returnServerConflict(res, message) {
+  res.status(409).send(message ? message : 'Server conflict.\n');
+  log.warn('Returned 409 Server Conflict.', message);
+}
+
+function logRequest(req, res, next) {
+  log.telemetry('Request received.', {
+    path: req.path,
+    method: req.method !== 'GET' ? req.method : undefined,
+    query: Object.keys(req.query).length > 0 ? req.query : undefined,
+    body: req.body,
+    ip: req.ip
+  });
+  next();
+}
+
+// TODO https://nodejs.org/api/process.html
+process.on('uncaughtException', e => {
+  log.error('Uncaught exception.', e);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  log.error('Unhandled rejection.', { reason, promise });
+});
+
+process.on('exit', () => {
+  log.fatal('Process is exiting, so long and thanks for all the fish.');
+});
+
+// Catch Ctrl+C
+process.on('SIGINT', () => {
+  log.warn('Got SIGINT. Exiting...');
+  process.exit();
+});
