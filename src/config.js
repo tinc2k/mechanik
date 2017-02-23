ef6344c83df4bedafde1a1f9f6233e0ccdc97840
Tin Crnkovic <tinc2k@gmail.com>
2017-02-23 20:33:11 +0100

    Config linting

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@
 const development = {
   isDevelopment: true, // for config.isDevelopment
   SERVER: {
-    api_version: 0.01, 
+    api_version: 0.01,
     cluster: true, // enable server clustering
     force_https: false // force/redirect to HTTPS
     // certificate paths are not required, as (local) development environment doesn't run in HTTPS
   },
-  REDIS: { 
+  REDIS: {
     host: 'localhost',
     port: 6379
   },
@@ -30,7 +30,7 @@ const integration = {
     certificate_path: 'certs/int/fullchain.pem',
     certificate_key_path: 'certs/int/privkey.pem'
   },
-  REDIS: { 
+  REDIS: {
     host: 'localhost',
     port: 6379
   },
@@ -44,7 +44,7 @@ const integration = {
 };
 
 const production = {
-  isProduction: true,  // for config.isProduction
+  isProduction: true, // for config.isProduction
   SERVER: {
     api_version: 0.01,
     cluster: true,
@@ -52,7 +52,7 @@ const production = {
     certificate_path: 'certs/prod/fullchain.pem',
     certificate_key_path: 'certs/prod/privkey.pem'
   },
-  REDIS: { 
+  REDIS: {
     host: 'localhost',
     port: 6379
   },
@@ -65,11 +65,10 @@ const production = {
   }
 };
 
-
-
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'production')	{
   module.exports = production;
-else if (process.env.NODE_ENV === 'integration')
+} else if (process.env.NODE_ENV === 'integration')	{
   module.exports = integration;
-else
+} else {
   module.exports = development;
+}
