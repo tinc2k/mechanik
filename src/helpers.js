d158d4e12db8252dc45e5f6d7bf62f45c963aa47
Tin Crnkovic <tinc2k@gmail.com>
2017-02-23 18:11:26 +0100

    Helpers linting

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,16 @@
 'use strict';
 
+const crypto = require('crypto');
+
 function getRandomHex(bytes, uppercase) {
   let t = crypto.randomBytes(bytes).toString('hex');
   return uppercase ? t.toUpperCase() : t;
 }
 
 function maybe(percent) {
-  if (Math.random() < percent/100)
+  if (Math.random() < percent / 100) {
     return true;
+  }
   return false;
 }
 
