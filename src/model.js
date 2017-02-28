d3bf0e46279718a7cd80ace3e2dc0c8992eeec95
Tin Crnkovic <tinc2k@gmail.com>
2017-02-28 01:27:46 +0100

    First draft models: Role, User, Token.

---
+++ src/model.js
@@ -0,0 +1,100 @@
+'use strict';
+
+const crypto = require('crypto');
+const moment = require('moment-timezone');
+
+const log = require('./logger')('Model');
+const helpers = require('./helpers');
+const config = require('./config')(process.env.NODE_ENV);
+
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    host: `${config.Postgres.host}:${config.Postgres.port}`,
+    user: config.Postgres.user,
+    password: config.Postgres.password,
+    database: config.Postgres.database,
+    charset: 'utf8'
+  },
+  pool: config.Postgres.pool,
+  migrations: { tableName: 'knex_migrations'}
+  //debug: true
+});
+const bookshelf = require('bookshelf')(knex);
+
+
+// https://github.com/tgriesser/bookshelf/wiki/Plugin:-Virtuals
+bookshelf.plugin('virtuals');
+
+// https://github.com/tgriesser/bookshelf/wiki/Plugin:-Visibility
+bookshelf.plugin('visibility');
+
+
+const Enums = {
+  Role: {
+    Administrator: 1,
+    Moderator: 2,
+    User: 3
+  }
+};
+
+var User = bookshelf.Model.extend({
+  tableName: 'User',
+  hasTimestamps: ['createdAt', 'updatedAt'],
+  hidden: ['passwordSalt','passwordHash'],
+  validTokens() {
+    return this.hasMany(Token, 'userId').query(q => {
+      // unexpired tokens only
+      q.where('expiresAt', '<', moment.utc().toISOString());
+    });
+  },
+  virtuals: {
+    isAdministrator() {
+      return this.get('role') === Enums.Role.Administrator;
+    },
+    isModerator() {
+      return this.get('role') === Enums.Role.Moderator;
+    },
+    isInfluencer() {
+      return this.get('role') === Enums.Role.Influencer;
+    }
+  }
+}, {
+  generatePasswordHash(salt, password) {
+    let str = salt + password;
+    log.verbose('Generating password hash.', str);
+    let hash = crypto.createHash('sha256').update(str).digest('hex');
+    //_logger.verbose('Generated hash', hash);
+    return hash;
+  },
+  toPublicJSON(i) {
+    return {
+      id: i.id,
+      username: i.username,
+      role: i.role,
+      email: i.email,
+      isActive: i.isActive
+    };
+  }
+});
+
+
+var Token = bookshelf.Model.extend({
+  tableName: 'Token',
+  hasTimestamps: ['createdAt', 'updatedAt'],
+  user() {
+    return this.belongsTo(User, 'userId');
+  }
+}, {
+  generateApiToken() {
+    let token = helpers.getRandomHex(32);
+    log.verbose('Generated new API token.', token);
+    return token;
+  }
+});
+
+module.exports = {
+  Role,
+  User,
+  Token
+};
