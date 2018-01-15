22a7f3a783974277bf33ded84028eb8fc17da93b
Tin Crnkovic <tinc2k@gmail.com>
2018-01-15 20:47:36 +0100

    Updated client to load React component into #app.

---
+++ src/client/client.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import ReactComponent from './ReactComponent';
+
+render(<ReactComponent title={'Cheers'}/>, document.getElementById('app'));
