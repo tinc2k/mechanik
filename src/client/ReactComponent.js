403acf774678ef99ccf5e41af843bf40fd0be000
Tin Crnkovic <tinc2k@gmail.com>
2018-01-15 20:48:37 +0100

    React component rebuild to ES6 style.

--- src/client/ReactComponent.js
+++ src/client/ReactComponent.js
@@ -2,12 +2,23 @@
 
 const React = require('react');
 
-const Header = React.createClass({
+class ReactComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false
+    };
+    this.render = this.render.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount() {
+    console.log('[ReactComponent] Mounted.');
+  }
   render() {
     return (
       <header id='header'><h1>{this.props.title}</h1></header>
     );
   }
-});
+}
 
-module.exports = Header;
+module.exports = ReactComponent;
